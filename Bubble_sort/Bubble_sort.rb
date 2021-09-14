ac10e91d8876f49b0df60c7493078df01115791d
Nami <mando.lin925@gmail.com>
2021-09-15 00:46:05 +0900

    rubocopを導入して警告内容を反映

--- Bubble_sort/Bubble_sort.rb
+++ Bubble_sort/Bubble_sort.rb
@@ -1,24 +1,19 @@
 def bubble_sort(a)
+  t = 1
+  while t <= a.length - 1
 
-    t = 1    
-    while t <= a.length - 1
-  
-      i = 0
-      while i <= a.length - t - 1
-    
-        if a[i] > a[i+1]
-          a[i], a[i+1] = a[i+1], a[i]
-        end
-    
-        i += 1
-      end
-    
-      t += 1
+    i = 0
+    while i <= a.length - t - 1
+
+      a[i], a[i + 1] = a[i + 1], a[i] if a[i] > a[i + 1]
+
+      i += 1
     end
-    
-    return(a)
-  end  
 
-p bubble_sort([1, 3, 2])  
-p bubble_sort([5, 1, 10, 4])  
-p bubble_sort([10, 8, 3, 5, 2, 4, 11, 18, 20, 33])  
+    t += 1
+  end
+
+  a
+end
+
+p bubble_sort([10, 8, 3, 5, 2, 4, 11, 18, 20, 33])
